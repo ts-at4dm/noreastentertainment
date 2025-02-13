6c1cf84a6fe36e5e330472d439bf6c3f3cc5075b
ts-at4dm <terran.stone@maine.edu>
2025-02-13 16:49:39 -0500

    Added mobile navbar

--- scripts/script.js
+++ scripts/script.js
@@ -40,3 +40,11 @@ function updateStates() {
         });
     } 
 }
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
