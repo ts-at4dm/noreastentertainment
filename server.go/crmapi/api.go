8ce4ffc56e37eddc75dbea8ed808518961cd4422
ts-at4dm <terran.stone@maine.edu>
2025-03-27 19:12:59 -0400

    connectDB func added

--- server.go/crmapi/api.go
+++ server.go/crmapi/api.go
@@ -1,1 +1,32 @@
 package crmapi
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+
+func connectDB() *sql.DB {
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbName
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
